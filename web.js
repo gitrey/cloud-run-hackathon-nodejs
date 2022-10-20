4e47c6ca78f59f212759cf39cdfd1148b2e0d85f
Andrey <gitrey@users.noreply.github.com>
2022-10-20 14:16:27 -0400

    Update web.js

--- web.js
+++ web.js
@@ -10,8 +10,14 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   console.log(JSON.stringify(req.headers));
-  console.log(req.body);
+  //console.log(req.body);
   const moves = ['F', 'T', 'L', 'R'];
+  
+  const arena = JSON.parse(req.body);
+  
+  console.log('arena.dims[0]', arena.dims[0]);
+  console.log('arena.dims[1]', arena.dims[1]);
+  console.log("my state:", arena.state['https://a-a5fddfpbyq-uc.a.run.app']);
   
   res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
