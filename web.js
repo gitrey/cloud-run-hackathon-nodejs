8ed3c08b839d88c5afb7b57a65c4bbef47c22827
Andrey <gitrey@users.noreply.github.com>
2022-10-20 14:47:37 -0400

    Update web.js

--- web.js
+++ web.js
@@ -13,6 +13,8 @@ app.post('/', function (req, res) {
   const moves = ['F', 'T', 'L', 'R'];
   
     const arena = req.body.arena;
+    
+//    const myPosition = arena.state['https://foo.com'];
     const myPosition = arena.state['https://a-a5fddfpbyq-uc.a.run.app'];
   
    console.log('arena.dims[0]', arena.dims[0]);
@@ -20,6 +22,32 @@ app.post('/', function (req, res) {
    console.log('myPosition', myPosition);
    
    for (var player in arena.state) {
+       var p = arena.state[player];
+
+    if (myPosition.x+1 == p.x &&
+        myPosition.y+1 == p.y) {
+            return  res.send('T');
+    }
+
+    if (myPosition.x-1 == p.x &&
+        myPosition.y-1 == p.y) {
+            return res.send('T');
+    }
+
+    if (myPosition.x == p.x &&
+        myPosition.y-1 == p.y) {
+            return res.send('T');
+    }
+
+    if (myPosition.x-1 == p.x &&
+        myPosition.y == p.y) {
+            return res.send('T');
+    }
+
+    if (myPosition.x == p.x &&
+        myPosition.y == p.y) {
+            return res.send('T');
+    }
     console.log(arena.state[player]);
     console.log(arena.state[player].x);
     console.log(arena.state[player].y);
