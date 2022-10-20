333778f23cd09fd080378b4fdafcafb762f4ba6b
Andrey <gitrey@users.noreply.github.com>
2022-10-20 14:41:40 -0400

    Update web.js

--- web.js
+++ web.js
@@ -13,16 +13,17 @@ app.post('/', function (req, res) {
   const moves = ['F', 'T', 'L', 'R'];
   
     const arena = req.body.arena;
-    const myPosition = arena.state['https://foo.com'];
+    const myPosition = arena.state['https://a-a5fddfpbyq-uc.a.run.app'];
   
    console.log('arena.dims[0]', arena.dims[0]);
    console.log('arena.dims[1]', arena.dims[1]);
    console.log('myPosition', myPosition);
+   
    for (var player in arena.state) {
+    console.log(arena.state[player]);
     console.log(arena.state[player].x);
     console.log(arena.state[player].y);
    }
-   console.log("my state:", arena.state['https://foo.com']);
 
 
    
